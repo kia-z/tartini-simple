32a46269b362fb8e40ab19b0812cb1329d443868
kia-z <chiara.zuanni@gmail.com>
2025-04-08 16:21:24 +0200

    update data model and forms

---
+++ js/form_people.js
@@ -0,0 +1,31 @@
+document.getElementById("people_form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    // remember! the first name is the one in google spreadsheet, the second one is the ID in the html form
+    const formPeople = {
+        action: "add", 
+        name: document.getElementById("person_name").value,
+        alternative_names: document.getElementById("person_altName").value,
+        identifiers: document.getElementById("person_ids").value,
+        roles: document.getElementById("person_role").value,
+        birth_date: document.getElementById("birthDate").value,
+        birth_place: document.getElementById("birthPlace").value,
+        death_date: document.getElementById("deathDate").value,
+        death_place: document.getElementById("deathPlace").value,
+        sources: document.getElementById("person_reference").value
+    };
+
+    fetch("https://script.google.com/macros/s/AKfycbyEzOA4r-ReJaySgx-FJhQAm4fxKtKlrhKWSzzrJNvkmuD8Suc4YO2YbeYa9uoOJUZGCw/exec", {
+        method: "POST",
+        mode: "no-cors",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formPeople),
+    })
+    .then(() => {
+        document.getElementById("responseMessageAdd").innerText = "Data submitted successfully!";
+        document.getElementById("people_form").reset();
+    })
+    .catch(() => {
+        document.getElementById("responseMessageAdd").innerText = "Error submitting data.";
+    });
+});
---
+++ js/form_works.js
@@ -0,0 +1,31 @@
+document.getElementById("works_form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    // remember! the first name is the one in google spreadsheet, the second one is the ID in the html form
+    const formWorks = {
+        action: "add", 
+        title: document.getElementById("title").value,
+        type: document.getElementById("type").value,
+        subtype: document.getElementById("subtype").value,
+        start_date: document.getElementById("start_date").value,
+        end_date: document.getElementById("end_date").value,
+        people_involved: document.getElementById("people_involved").value,
+        performances: document.getElementById("performances").value,
+        places: document.getElementById("places").value,
+        sources: document.getElementById("sources").value
+    };
+
+    fetch("https://script.google.com/macros/s/AKfycbyEzOA4r-ReJaySgx-FJhQAm4fxKtKlrhKWSzzrJNvkmuD8Suc4YO2YbeYa9uoOJUZGCw/exec", {
+        method: "POST",
+        mode: "no-cors",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formWorks),
+    })
+    .then(() => {
+        document.getElementById("responseMessageAdd").innerText = "Data submitted successfully!";
+        document.getElementById("works_form").reset();
+    })
+    .catch(() => {
+        document.getElementById("responseMessageAdd").innerText = "Error submitting data.";
+    });
+});
